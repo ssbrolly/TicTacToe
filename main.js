1b3306cd430ab4d3161a5e96d2c94a39d59ab8e5
Usong Cho <silvermouse4012@gmail.com>
2019-02-13 20:20:35 -0800

    added toggle to blue

--- main.js
+++ main.js
@@ -30,32 +30,47 @@
 // for (let i = 0; i < 3; i++) {
 //   let xpos = i * 20;
 //   for (let y = 0; y < 3; y++) {
-//     let ypos = y * 20;
-//     createBox(xpos, ypos);
-//   } 
-// };
-
-
+    //     let ypos = y * 20;
+    //     createBox(xpos, ypos);
+    //   } 
+    // };
+    
 let app = document.querySelector('.app');
+let activePlayer = 0;
 
 function startGame(xpos, ypos) {
     let box = document.createElement('div');
-    box.classList = 'box green';
+    box.className = 'box green';
     box.style.left = xpos + 'px';
     box.style.top = ypos + 'px';
     app.appendChild(box);
-
+    
     box.addEventListener('click', (e) => {
-        let newBox = e.target;
-        newBox.classList.toggle('red');
-        newBox.classList.toggle('green');
-    })
+        let thisBox = e.target;
+        if (activePlayer === 1) {
+            thisBox.classList.toggle('red');
+            thisBox.classList.remove('blue');
+            // thisBox.classList.toggle('green');
+            nextPlayer();
+        } else {
+            thisBox.classList.toggle('blue');
+            thisBox.classList.remove('red');
+            // thisBox.classList.toggle('green');
+            nextPlayer();
+        };
+    });
 };
 
-for (let i = 0; i < 5; i++) {
+function nextPlayer() {
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+};
+
+
+
+for (let i = 0; i < 3; i++) {
     let xpos = i * 20;
-    for (let y = 0; y < 5; y++) {
+    for (let y = 0; y < 3; y++) {
         let ypos = y * 20;
         startGame(xpos, ypos);
     }
-};
+}
