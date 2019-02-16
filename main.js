704ae14387d68bb64e2f2c1b61beefed5273461a
Usong Cho <silvermouse4012@gmail.com>
2019-02-16 14:04:42 -0800

    removed practice

--- main.js
+++ main.js
@@ -75,37 +75,4 @@
 //     }
 // }
 
-let app = document.querySelector('.app');
-let activePlayer = 0;
-
-function startGame(xpos, ypos) {
-    let box = document.createElement('div');
-    box.className = 'box green';
-    box.style.left = xpos + 'px';
-    box.style.top = ypos + 'px';
-    app.appendChild(box);
-
-    box.addEventListener('click', (e) => {
-        let thisBox = e.target;
-        if (activePlayer === 0) {
-            thisBox.classList.toggle('red');
-            nextPlayer();
-        } else {
-            thisBox.classList.toggle('blue');
-            nextPlayer();
-        };
-    });
-};
-
-function nextPlayer() {
-    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-};
-
-for (let i = 0; i < 5; i++) {
-    let xpos = i * 20;
-    for(let y = 0; y < 5; y++) {
-        let ypos = y * 20;
-        startGame(xpos, ypos);
-    };
-};
 
