0d7ea5ca57bb5d0678bb8ce9dc9a6a021bf5ed45
Usong Cho <silvermouse4012@gmail.com>
2019-02-15 20:41:34 -0800

    practice

--- main.js
+++ main.js
@@ -35,6 +35,46 @@
     //   } 
     // };
     
+// let app = document.querySelector('.app');
+// let activePlayer = 0;
+
+// function startGame(xpos, ypos) {
+//     let box = document.createElement('div');
+//     box.className = 'box green';
+//     box.style.left = xpos + 'px';
+//     box.style.top = ypos + 'px';
+//     app.appendChild(box);
+    
+//     box.addEventListener('click', (e) => {
+//         let thisBox = e.target;
+//         if (activePlayer === 1) {
+//             thisBox.classList.toggle('red');
+//             thisBox.classList.remove('blue');
+//             // thisBox.classList.toggle('green');
+//             nextPlayer();
+//         } else {
+//             thisBox.classList.toggle('blue');
+//             thisBox.classList.remove('red');
+//             // thisBox.classList.toggle('green');
+//             nextPlayer();
+//         };
+//     });
+// };
+
+// function nextPlayer() {
+//     activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+// };
+
+
+
+// for (let i = 0; i < 3; i++) {
+//     let xpos = i * 20;
+//     for (let y = 0; y < 3; y++) {
+//         let ypos = y * 20;
+//         startGame(xpos, ypos);
+//     }
+// }
+
 let app = document.querySelector('.app');
 let activePlayer = 0;
 
@@ -44,18 +84,14 @@ function startGame(xpos, ypos) {
     box.style.left = xpos + 'px';
     box.style.top = ypos + 'px';
     app.appendChild(box);
-    
+
     box.addEventListener('click', (e) => {
         let thisBox = e.target;
-        if (activePlayer === 1) {
+        if (activePlayer === 0) {
             thisBox.classList.toggle('red');
-            thisBox.classList.remove('blue');
-            // thisBox.classList.toggle('green');
             nextPlayer();
         } else {
             thisBox.classList.toggle('blue');
-            thisBox.classList.remove('red');
-            // thisBox.classList.toggle('green');
             nextPlayer();
         };
     });
@@ -65,12 +101,11 @@ function nextPlayer() {
     activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
 };
 
-
-
-for (let i = 0; i < 3; i++) {
+for (let i = 0; i < 5; i++) {
     let xpos = i * 20;
-    for (let y = 0; y < 3; y++) {
+    for(let y = 0; y < 5; y++) {
         let ypos = y * 20;
         startGame(xpos, ypos);
-    }
-}
+    };
+};
+
