162801dac3aee21f3c99f7165e5a3f2e703f4fb3
Usong Cho <silvermouse4012@gmail.com>
2019-04-03 19:03:05 -0700

    practice

--- main.js
+++ main.js
@@ -39,37 +39,3 @@
 //     };
 // };
 
-
-let app = document.querySelector('.app');
-let activePlayer = 0;
-
-function startGame(xpos, ypos) {
-    let box = document.createElement('div');
-    box.className = 'box green';
-    box.style.left = xpos + 'px';
-    box.style.top = ypos + 'px';
-    app.appendChild(box);
-
-    box.addEventListener('click', (e) => {
-        let thisBox = e.target;
-        if (activePlayer === 0) {
-            thisBox.classList.toggle('red');
-            nextPlayer();
-        } else if (activePlayer === 1) {
-            thisBox.classList.toggle('blue');
-            nextPlayer();
-        }
-    })
-};
-
-function nexPlayer() {
-    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-}
-
-for (let i = 0; i < 5; i++) {
-    let xpos = i * 20;
-    for (let y = 0; y < 5; y++) {
-        let ypos = y * 20;
-        startGame(xpos, ypos);
-    };
-};
